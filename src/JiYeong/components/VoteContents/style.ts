994e27e922758962395e4e5e33eeb2fc4e27d2bf
NaveOWO <pianowell96@gmail.com>
2022-07-05 18:41:02 +0900

    [ design ] styled-component

---
+++ src/JiYeong/components/VoteContents/style.ts
@@ -0,0 +1,41 @@
+import styled from "styled-components";
+
+export const VoteContentsContainer = styled.section`
+  margin-left: 200px;
+  margin-top: 30px;
+`;
+export const VoteDiscription = styled.strong`
+  font-size: 20px;
+  margin-left: 20px;
+`;
+export const VoterCount = styled.p`
+  font-size: 15px;
+  color: gray;
+  margin-top: 10px;
+  margin-left: 20px;
+`;
+
+export const OptionListContainer = styled.ul`
+  border: solid 1px black;
+  padding: 20px;
+  margin: 20px;
+`;
+export const OptionListContent = styled.li`
+  border: solid 1px black;
+  height: 100px;
+  margin: 10px;
+  display: flex;
+  align-items: center;
+`;
+export const OptionListMark = styled.span`
+  margin-right: 20px;
+  margin-left: 20px;
+  font-size: 15px;
+`;
+export const OptionListText = styled.strong`
+  font-size: 30px;
+`;
+export const VotedPercent = styled.div`
+  margin-right: 0px;
+  font-size: 20px;
+`;
