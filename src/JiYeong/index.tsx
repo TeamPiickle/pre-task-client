aebe90c379754058384f886d2cfc8f3f76ec696a
NaveOWO <pianowell96@gmail.com>
2022-07-05 18:39:34 +0900

    [ feat ] 투표기능 구현

--- src/JiYeong/index.tsx
+++ src/JiYeong/index.tsx
@@ -1,3 +1,13 @@
+import styled from "styled-components";
+
+import UserProfile from "./components/UserProfile";
+import VoteContents from "./components/VoteContents";
+
 export default function index() {
-  return <div>index</div>;
+  return (
+    <>
+      <UserProfile></UserProfile>
+      <VoteContents></VoteContents>
+    </>
+  );
 }
