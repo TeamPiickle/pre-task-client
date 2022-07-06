6ad765a72b7e03dd6897f53373478fa20c22faed
SNUPI <joohaem@daum.net>
2022-07-06 20:13:55 +0900

    [ refactor ] 객체 합치기

--- src/Filter/index.tsx
+++ src/Filter/index.tsx
@@ -1,41 +1,47 @@
-import { useState } from "react";
+import React, { useState } from "react";
 
 import { St } from "./style";
 
 type FilterTag = {
   id: string;
   name: "남" | "여" | "10대" | "20대" | "30대";
-  type: "성별" | "연령대";
 };
 
-const filterGenderTags: FilterTag[] = [
-  {
-    id: "1",
-    name: "남",
-    type: "성별",
-  },
-  {
-    id: "2",
-    name: "여",
-    type: "성별",
-  },
-];
+type FilterTags = {
+  type: string;
+  tags: FilterTag[];
+};
 
-const filterAgeTags: FilterTag[] = [
+const filterTags: FilterTags[] = [
   {
-    id: "3",
-    name: "10대",
-    type: "연령대",
+    type: "성별",
+    tags: [
+      {
+        id: "1",
+        name: "남",
+      },
+      {
+        id: "2",
+        name: "여",
+      },
+    ],
   },
   {
-    id: "4",
-    name: "20대",
     type: "연령대",
-  },
-  {
-    id: "5",
-    name: "30대",
-    type: "연령대",
+    tags: [
+      {
+        id: "3",
+        name: "10대",
+      },
+      {
+        id: "4",
+        name: "20대",
+      },
+      {
+        id: "5",
+        name: "30대",
+      },
+    ],
   },
 ];
 
@@ -50,22 +56,19 @@ export default function Filter() {
 
   return (
     <section>
-      <St.FilterTitle>성별</St.FilterTitle>
-      <St.FilterTagsWrapper>
-        {filterGenderTags.map((tag) => (
-          <St.FilterTag key={tag.id} isactive={checkedTags.has(tag.id)} onClick={() => toggleTagButton(tag.id)}>
-            {tag.name}
-          </St.FilterTag>
-        ))}
-      </St.FilterTagsWrapper>
-      <St.FilterTitle>연령대</St.FilterTitle>
-      <St.FilterTagsWrapper>
-        {filterAgeTags.map((tag) => (
-          <St.FilterTag key={tag.id} isactive={checkedTags.has(tag.id)} onClick={() => toggleTagButton(tag.id)}>
-            {tag.name}
-          </St.FilterTag>
-        ))}
-      </St.FilterTagsWrapper>
+      {filterTags.map((filterTag, idx) => (
+        <React.Fragment key={`filter-${idx}`}>
+          <St.FilterTitle>{filterTag.type}</St.FilterTitle>
+          <St.FilterTagsWrapper>
+            {filterTag.tags.map((tag) => (
+              <St.FilterTag key={tag.id} isactive={checkedTags.has(tag.id)} onClick={() => toggleTagButton(tag.id)}>
+                {tag.name}
+              </St.FilterTag>
+            ))}
+          </St.FilterTagsWrapper>
+        </React.Fragment>
+      ))}
+
       <St.SubmitBtn onClick={() => window.alert([...checkedTags])}>제출하기</St.SubmitBtn>
     </section>
   );
