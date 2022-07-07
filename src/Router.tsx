94e7e3c443bd5624579f3ce644ebf8da6f837c75
SNUPI <joohaem@daum.net>
2022-07-07 11:08:03 +0900

    [ feat ] add swipe page

--- src/Router.tsx
+++ src/Router.tsx
@@ -6,6 +6,7 @@ import HyeEun from "./HyeEun";
 import JiYeong from "./JiYeong";
 import Main from "./Main";
 import NaYeon from "./NaYeon";
+import Swipe from "./Swipe";
 
 export default function Router() {
   return (
@@ -16,6 +17,7 @@ export default function Router() {
         <Route path="/jy" element={<JiYeong />} />
         <Route path="/ny" element={<NaYeon />} />
         <Route path="/filter" element={<Filter />} />
+        <Route path="/swipe" element={<Swipe />} />
         <Route path="*" element={<Error404 />} />
       </Routes>
     </BrowserRouter>
