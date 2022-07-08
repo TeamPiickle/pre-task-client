1a6c2388093380f5bd56ddfb4f04e4b44f7239be
SNUPI <joohaem@daum.net>
2022-07-08 20:45:02 +0900

    [ bug ] swiper js 에 클릭 동작 이벤트 감지 불가 확인

--- src/Swipe/SwipeSlider/index.tsx
+++ src/Swipe/SwipeSlider/index.tsx
@@ -1,8 +1,18 @@
+/*
+마지막 편집자: 22-07-08 joohaem
+변경사항 및 참고:
+  - swiper 와 motion 컴포넌트 분리
+    
+고민점:
+  - 
+*/
+
 /* eslint-disable import/no-unresolved */
 // swiper js v7 이상의 에러
 import "swiper/css";
 import "./styles.css";
 
+import { useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 
 import { St } from "./styled";
@@ -11,15 +21,18 @@ export default function SwipeSlider() {
   return (
     <St.Root>
       <Swiper slidesPerView={"auto"} spaceBetween={44} centeredSlides={true} grabCursor={true}>
-        <SwiperSlide>Slide 1</SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>
-        <SwiperSlide>Slide 5</SwiperSlide>
-        <SwiperSlide>Slide 6</SwiperSlide>
-        <SwiperSlide>Slide 7</SwiperSlide>
-        <SwiperSlide>Slide 8</SwiperSlide>
-        <SwiperSlide>Slide 9</SwiperSlide>
+        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((el, i) => {
+          console.log(i);
+          return (
+            <div key={i} aria-hidden="true" onClick={(e) => console.log(e)}>
+              <SwiperSlide key={`slidee-${i}`}>
+                <div key={i} aria-hidden="true" onClick={(e) => console.log(e.target)}>
+                  Slide {i}
+                </div>
+              </SwiperSlide>
+            </div>
+          );
+        })}
       </Swiper>
     </St.Root>
   );
