31685fc42bc3f82a1978ee14edf4850a27c4b196
NYeonK <skdus728@gmail.com>
2022-07-08 21:01:05 +0900

    [ refactor ] css 파일 삭제 후, style.ts에 추가

--- src/NaYeon/slider_bar/components/slider.tsx
+++ src/NaYeon/slider_bar/components/slider.tsx
@@ -1,6 +1,5 @@
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import "../style/progress.css";
 
 import React, { Component } from "react";
 import Slider from "react-slick";
@@ -21,44 +20,46 @@ export default class SimpleSlider extends Component {
     return (
       <St.MultipleSlider>
         <St.Title> ⭕ 킹받는 넌센스 퀴즈 ❌ </St.Title>
-        <Slider {...settings}>
-          <St.ImageWrapper>
-            <St.Image
-              src="https://every7hing.com/wp-content/uploads/2022/02/%E1%84%89%E1%85%B3%E1%86%AF%E1%84%85%E1%85%A1%E1%84%8B%E1%85%B5%E1%84%83%E1%85%B32-4.jpeg"
-              alt="1번문제"
-            />
-          </St.ImageWrapper>
-          <St.ImageWrapper>
-            <St.Image
-              src="https://every7hing.com/wp-content/uploads/2022/02/%E1%84%89%E1%85%B3%E1%86%AF%E1%84%85%E1%85%A1%E1%84%8B%E1%85%B5%E1%84%83%E1%85%B33-4.jpeg"
-              alt="1번정답"
-            />
-          </St.ImageWrapper>
-          <St.ImageWrapper>
-            <St.Image
-              src="https://every7hing.com/wp-content/uploads/2022/02/%E1%84%89%E1%85%B3%E1%86%AF%E1%84%85%E1%85%A1%E1%84%8B%E1%85%B5%E1%84%83%E1%85%B34-2.jpeg"
-              alt="2번문제"
-            />
-          </St.ImageWrapper>
-          <St.ImageWrapper>
-            <St.Image
-              src="https://every7hing.com/wp-content/uploads/2022/02/%E1%84%89%E1%85%B3%E1%86%AF%E1%84%85%E1%85%A1%E1%84%8B%E1%85%B5%E1%84%83%E1%85%B35-2.jpeg"
-              alt="2번정답"
-            />
-          </St.ImageWrapper>
-          <St.ImageWrapper>
-            <St.Image
-              src="https://every7hing.com/wp-content/uploads/2022/02/%E1%84%89%E1%85%B3%E1%86%AF%E1%84%85%E1%85%A1%E1%84%8B%E1%85%B5%E1%84%83%E1%85%B36-2.jpeg"
-              alt="3번문제"
-            />
-          </St.ImageWrapper>
-          <St.ImageWrapper>
-            <St.Image
-              src="https://every7hing.com/wp-content/uploads/2022/02/%E1%84%89%E1%85%B3%E1%86%AF%E1%84%85%E1%85%A1%E1%84%8B%E1%85%B5%E1%84%83%E1%85%B37-1.jpeg"
-              alt="3번정답"
-            />
-          </St.ImageWrapper>
-        </Slider>
+        <St.SliderWrapper>
+          <Slider {...settings}>
+            <St.ImageWrapper>
+              <St.Image
+                src="https://every7hing.com/wp-content/uploads/2022/02/%E1%84%89%E1%85%B3%E1%86%AF%E1%84%85%E1%85%A1%E1%84%8B%E1%85%B5%E1%84%83%E1%85%B32-4.jpeg"
+                alt="1번문제"
+              />
+            </St.ImageWrapper>
+            <St.ImageWrapper>
+              <St.Image
+                src="https://every7hing.com/wp-content/uploads/2022/02/%E1%84%89%E1%85%B3%E1%86%AF%E1%84%85%E1%85%A1%E1%84%8B%E1%85%B5%E1%84%83%E1%85%B33-4.jpeg"
+                alt="1번정답"
+              />
+            </St.ImageWrapper>
+            <St.ImageWrapper>
+              <St.Image
+                src="https://every7hing.com/wp-content/uploads/2022/02/%E1%84%89%E1%85%B3%E1%86%AF%E1%84%85%E1%85%A1%E1%84%8B%E1%85%B5%E1%84%83%E1%85%B34-2.jpeg"
+                alt="2번문제"
+              />
+            </St.ImageWrapper>
+            <St.ImageWrapper>
+              <St.Image
+                src="https://every7hing.com/wp-content/uploads/2022/02/%E1%84%89%E1%85%B3%E1%86%AF%E1%84%85%E1%85%A1%E1%84%8B%E1%85%B5%E1%84%83%E1%85%B35-2.jpeg"
+                alt="2번정답"
+              />
+            </St.ImageWrapper>
+            <St.ImageWrapper>
+              <St.Image
+                src="https://every7hing.com/wp-content/uploads/2022/02/%E1%84%89%E1%85%B3%E1%86%AF%E1%84%85%E1%85%A1%E1%84%8B%E1%85%B5%E1%84%83%E1%85%B36-2.jpeg"
+                alt="3번문제"
+              />
+            </St.ImageWrapper>
+            <St.ImageWrapper>
+              <St.Image
+                src="https://every7hing.com/wp-content/uploads/2022/02/%E1%84%89%E1%85%B3%E1%86%AF%E1%84%85%E1%85%A1%E1%84%8B%E1%85%B5%E1%84%83%E1%85%B37-1.jpeg"
+                alt="3번정답"
+              />
+            </St.ImageWrapper>
+          </Slider>
+        </St.SliderWrapper>
       </St.MultipleSlider>
     );
   }
--- src/NaYeon/slider_bar/style.ts
+++ src/NaYeon/slider_bar/style.ts
@@ -13,7 +13,7 @@ export const St = {
   `,
 
   Title: styled.h1`
-    font-size: 2rem;
+    font-size: 3rem;
     font-weight: bold;
     color: white;
     padding: 3rem;
@@ -32,5 +32,59 @@ export const St = {
     height: 450px;
     width: 50%;
     box-sizing: border-box;
+  `,
+
+  SliderWrapper: styled.div`
+    .slider-container {
+      text-align: center;
+      width: 100%;
+      max-width: 800px;
+      margin-left: auto;
+      position: relative;
+    }
+
+    .slider-container .range-slider {
+      width: 78%;
+      border: none;
+      box-shadow: none;
+      position: absolute;
+      top: 10px;
+      padding: 2px;
+    }
+    .slick-dots {
+      list-style: none;
+      padding: 0;
+      margin: auto;
+      align-items: center;
+      display: flex;
+      justify-content: center;
+      min-height: auto;
+    }
+    .slick-dots li {
+      margin: 0 -3px;
+      height: 1rem;
+      background-color: lightgray;
+      width: 13rem;
+      border-radius: 4px;
+    }
+    .slick-dots button {
+      border: none;
+      background: gray;
+      width: 100%;
+      font-size: 0;
+      border-radius: 4px;
+    }
+    .slick-dots .slick-active button {
+      background: white;
+      width: 100%;
+      border-radius: 4px;
+    }
+
+    .slick-dots li button:before {
+      width: 100%;
+      height: 1rem;
+      content: "";
+      line-height: 0px;
+    }
   `,
 };
