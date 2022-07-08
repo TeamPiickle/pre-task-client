fbd3bb40383d42b875354d5b782a869a17c7b205
SNUPI <joohaem@daum.net>
2022-07-08 15:40:46 +0900

    [ revert ] 다시 ... swiper v8

--- src/Swipe/SwipeSlider/index.tsx
+++ src/Swipe/SwipeSlider/index.tsx
@@ -1,40 +1,54 @@
 /* eslint-disable import/no-unresolved */
 // swiper js v7 이상의 에러
-import "swiper/swiper-bundle.min.css";
-import "swiper/swiper.min.css";
-import "swiper/components/navigation/navigation.min.css";
-import "swiper/components/pagination/pagination.min.css";
+import "swiper/css";
+import "swiper/css/pagination";
 
-import { useState } from "react";
 // import "./styles.css";
-import SwiperCore, { A11y, Navigation, Pagination } from "swiper";
+import { Pagination } from "swiper";
 import { Swiper, SwiperSlide } from "swiper/react";
 
 import { St } from "./styled";
 
-SwiperCore.use([Navigation, Pagination, A11y]);
-
 export default function SwipeSlider() {
-  // const [swiper, setSwiper] = useState<any>(null);
-  // const [idx, setIdx] = useState(0);
-
   return (
-    <St.Root>
-      <Swiper
-        spaceBetween={50}
-        slidesPerView={3}
-        onSwiper={(swiper) => console.log(swiper)}
-        onSlideChange={() => console.log("slide change")}>
-        <SwiperSlide>Slide 1</SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>
-        <SwiperSlide>Slide 5</SwiperSlide>
-        <SwiperSlide>Slide 6</SwiperSlide>
-        <SwiperSlide>Slide 7</SwiperSlide>
-        <SwiperSlide>Slide 8</SwiperSlide>
-        <SwiperSlide>Slide 9</SwiperSlide>
-      </Swiper>
-    </St.Root>
+    <div>
+      <St.Swiper
+        slidesPerView="auto"
+        spaceBetween={30}
+        centeredSlides={true}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[Pagination]}
+        className="mySwiper">
+        <SwiperSlide>
+          <St.SwiperSlide>Slide 1</St.SwiperSlide>
+        </SwiperSlide>
+        <SwiperSlide>
+          <St.SwiperSlide>Slide 2</St.SwiperSlide>
+        </SwiperSlide>
+        <SwiperSlide>
+          <St.SwiperSlide>Slide 3</St.SwiperSlide>
+        </SwiperSlide>
+        <SwiperSlide>
+          <St.SwiperSlide>Slide 4</St.SwiperSlide>
+        </SwiperSlide>
+        <SwiperSlide>
+          <St.SwiperSlide>Slide 5</St.SwiperSlide>
+        </SwiperSlide>
+        <SwiperSlide>
+          <St.SwiperSlide>Slide 6</St.SwiperSlide>
+        </SwiperSlide>
+        <SwiperSlide>
+          <St.SwiperSlide>Slide 7</St.SwiperSlide>
+        </SwiperSlide>
+        <SwiperSlide>
+          <St.SwiperSlide>Slide 8</St.SwiperSlide>
+        </SwiperSlide>
+        <SwiperSlide>
+          <St.SwiperSlide>Slide 9</St.SwiperSlide>
+        </SwiperSlide>
+      </St.Swiper>
+    </div>
   );
 }
--- src/Swipe/SwipeSlider/styled.ts
+++ src/Swipe/SwipeSlider/styled.ts
@@ -3,13 +3,6 @@ import styled from "styled-components";
 import { Swiper, SwiperSlide } from "swiper/react";
 
 export const St = {
-  Root: styled.main`
-    max-width: 48rem;
-    height: 100vh;
-    margin: 0 auto;
-    overflow: hidden;
-  `,
-
   Swiper: styled(Swiper)`
     height: 40rem;
 
