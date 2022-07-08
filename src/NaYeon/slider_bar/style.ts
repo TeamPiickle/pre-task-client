d7090d6ca95987530b2de7962b7b7dd12461ede0
NYeonK <skdus728@gmail.com>
2022-07-08 21:52:12 +0900

    [ design ] bar css 수정

--- src/NaYeon/slider_bar/style.ts
+++ src/NaYeon/slider_bar/style.ts
@@ -6,85 +6,94 @@ export const St = {
     background-color: black;
   `,
 
-  //silder
   MultipleSlider: styled.section`
     text-align: center;
     padding: 0 4rem;
   `,
 
   Title: styled.h1`
+    padding: 3rem;
+
     font-size: 3rem;
     font-weight: bold;
     color: white;
-    padding: 3rem;
   `,
 
+  //silde-image
   ImageWrapper: styled.div`
-    //height: 350px;
     width: 50%;
+
     border: 20px solid black;
     box-sizing: border-box;
   `,
 
   Image: styled.img`
     margin: auto;
-    object-fit: cover;
     height: 450px;
     width: 50%;
+
+    object-fit: cover;
     box-sizing: border-box;
   `,
 
+  //slide_bar
   SliderWrapper: styled.div`
     .slider-container {
       text-align: center;
+      position: relative;
+
+      margin-left: auto;
       width: 100%;
       max-width: 800px;
-      margin-left: auto;
-      position: relative;
     }
 
     .slider-container .range-slider {
+      position: absolute;
+      top: 10px;
+
+      padding: 2px;
       width: 78%;
       border: none;
       box-shadow: none;
-      position: absolute;
-      top: 10px;
-      padding: 2px;
     }
+
     .slick-dots {
-      list-style: none;
-      padding: 0;
-      margin: auto;
       align-items: center;
       display: flex;
       justify-content: center;
+
+      list-style: none;
+
+      padding: 0;
+      margin: auto;
       min-height: auto;
     }
+
     .slick-dots li {
       margin: 0 -3px;
       height: 1rem;
-      background-color: lightgray;
       width: 13rem;
+
+      background-color: gray;
       border-radius: 4px;
     }
+
     .slick-dots button {
       border: none;
-      background: gray;
       width: 100%;
-      font-size: 0;
-      border-radius: 4px;
     }
+
     .slick-dots .slick-active button {
-      background: white;
       width: 100%;
+      height: 1rem;
+
+      background: white;
       border-radius: 4px;
     }
 
     .slick-dots li button:before {
       width: 100%;
-      height: 1rem;
       content: "";
-      line-height: 0px;
     }
   `,
 };
