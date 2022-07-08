6ff5de19f5db6aecaebbdfbe70785721ac50b571
JY <pianowell96@gmail.com>
2022-07-08 17:55:33 +0900

    [ refactor ] 함수이름 컴포넌트명과 같게 변경

--- src/JiYeong/filterJY/index.tsx
+++ src/JiYeong/filterJY/index.tsx
@@ -1,13 +1,6 @@
 import styled from "styled-components";
 
-import UserProfile from "./components/UserProfile";
-import VoteContents from "./components/VoteContents";
-
-export default function index() {
-  return (
-    <>
-      <UserProfile></UserProfile>
-      <VoteContents></VoteContents>
-    </>
-  );
+import CategoryTitle from "./components/categoryTilte";
+export default function filterJY() {
+  return <CategoryTitle />;
 }
