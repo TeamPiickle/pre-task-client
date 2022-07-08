4fc249cbb97e86e1b24c187e666fb9fca818cf70
SNUPI <joohaem@daum.net>
2022-07-08 14:42:00 +0900

    [ fix ] React Strict 모드 해제

--- src/Swipe/SwipeSlider/index.tsx
+++ src/Swipe/SwipeSlider/index.tsx
@@ -1,31 +1,30 @@
 /* eslint-disable import/no-unresolved */
 // swiper js v7 이상의 에러
+import "swiper/swiper-bundle.min.css";
 import "swiper/swiper.min.css";
 import "swiper/components/navigation/navigation.min.css";
+import "swiper/components/pagination/pagination.min.css";
 
 import { useState } from "react";
 // import "./styles.css";
-import SwiperCore, { Navigation, Pagination } from "swiper";
+import SwiperCore, { A11y, Navigation, Pagination } from "swiper";
 import { Swiper, SwiperSlide } from "swiper/react";
 
 import { St } from "./styled";
 
+SwiperCore.use([Navigation, Pagination, A11y]);
+
 export default function SwipeSlider() {
-  const [swiper, setSwiper] = useState<any>(null);
-  const [idx, setIdx] = useState(0);
-
-  // SwiperCore.use([Navigation]);
-
-  // const swiperParams = {
-  //   navigation: true,
-  //   onSwiper: setSwiper,
-  //   onSlideChange: (e: any) => setIdx(e.activeIndex),
-  // };
+  // const [swiper, setSwiper] = useState<any>(null);
+  // const [idx, setIdx] = useState(0);
 
   return (
     <St.Root>
-      s
-      {/* <Swiper {...swiperParams}>
+      <Swiper
+        spaceBetween={50}
+        slidesPerView={3}
+        onSwiper={(swiper) => console.log(swiper)}
+        onSlideChange={() => console.log("slide change")}>
         <SwiperSlide>Slide 1</SwiperSlide>
         <SwiperSlide>Slide 2</SwiperSlide>
         <SwiperSlide>Slide 3</SwiperSlide>
@@ -35,7 +34,7 @@ export default function SwipeSlider() {
         <SwiperSlide>Slide 7</SwiperSlide>
         <SwiperSlide>Slide 8</SwiperSlide>
         <SwiperSlide>Slide 9</SwiperSlide>
-      </Swiper> */}
+      </Swiper>
     </St.Root>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -50,8 +50,10 @@ export const GlobalStyle = createGlobalStyle`
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
-  <React.StrictMode>
+  // <React.StrictMode>
+  <>
     <GlobalStyle />
     <App />
-  </React.StrictMode>,
+  </>,
+  // </React.StrictMode>,
 );
