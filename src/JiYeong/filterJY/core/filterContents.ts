558217fbbfc8893a5064b8319911892606e95ad8
JY <pianowell96@gmail.com>
2022-07-08 17:58:45 +0900

    [ feat ] filter 컴포넌트에서 사용하는 객체 생성

---
+++ src/JiYeong/filterJY/core/filterContents.ts
@@ -0,0 +1,17 @@
+export const filterContentsArr: { id: number; title: string; contents: string[] }[] = [
+  {
+    id: 0,
+    title: "Gender",
+    contents: ["Lady", "Man"],
+  },
+  {
+    id: 1,
+    title: "Age",
+    contents: ["10대", "20대", "30대", "40대"],
+  },
+  {
+    id: 2,
+    title: "Intimacy",
+    contents: ["어사", "쩜 친함", "찐친"],
+  },
+];
---
+++ src/JiYeong/filterJY/core/resultContents.ts
@@ -0,0 +1,50 @@
+export const resultContentsArr = [
+  {
+    content: "lady and 20",
+    gender: ["lady"],
+    age: ["20"],
+    intimacy: ["어사"],
+  },
+  {
+    content: "man and 30",
+    gender: ["man"],
+    age: ["30"],
+    intimacy: ["쩜 친함"],
+  },
+  {
+    content: "lady and 40",
+    gender: ["lady"],
+    age: ["20"],
+    intimacy: ["찐친"],
+  },
+  {
+    content: "lady and 2030",
+    gender: ["lady"],
+    age: ["20", "30"],
+    intimacy: ["어사", "쩜 친함"],
+  },
+  {
+    content: "man and 2040",
+    gender: ["man"],
+    age: ["20", "40"],
+    intimacy: ["어사", "쩜 친함", "찐친"],
+  },
+  {
+    content: "lady and 203040",
+    gender: ["lady"],
+    age: ["20", "30", "40"],
+    intimacy: ["어사", "찐친"],
+  },
+  {
+    content: "lady and man and 2030",
+    gender: ["lady", "man"],
+    age: ["20", "30"],
+    intimacy: ["쩜 친함"],
+  },
+  {
+    content: "lady and man and 20304050",
+    gender: ["lady", "man"],
+    age: ["20", "30", "40", "50"],
+    intimacy: ["찐친"],
+  },
+];
