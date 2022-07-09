9c0d116b98c3a7bd6462c2b9f9375775d6aabcac
Hennization <shy6295@ewhain.net>
2022-07-09 15:14:24 +0900

    [ fix ] handling 함수 상위 컴포넌트에 선언하기

--- src/HyeEun/filter/components/common/OptionButton/index.tsx
+++ src/HyeEun/filter/components/common/OptionButton/index.tsx
@@ -1,27 +1,17 @@
-import { useState } from "react";
-
 import { St } from "./style";
 
 interface OptionbuttonProps {
   content: string;
   submitSet: Set<string>;
-  setSubmitSet: React.Dispatch<React.SetStateAction<Set<string>>>;
+  handleSubmitSet: (content: string) => void;
 }
 
 export default function OptionButton(props: OptionbuttonProps) {
-  const { content, submitSet, setSubmitSet } = props;
-  const [isClicked, setIsClicked] = useState(false);
-
-  const handleClick = (content: string) => {
-    setIsClicked(!isClicked);
-    // submitSet에 content가 있으면 submitSet에서 삭제, 아니면 추가
-    const tempSubmitSet = new Set([...submitSet]);
-    tempSubmitSet.has(content) ? tempSubmitSet.delete(content) : tempSubmitSet.add(content);
-  };
+  const { content, submitSet, handleSubmitSet } = props;
 
   return (
     <>
-      <St.OptionButton isactive={isClicked} type="button" onClick={() => handleClick(content)}>
+      <St.OptionButton isactive={submitSet.has(content)} type="button" onClick={() => handleSubmitSet(content)}>
         {content}
       </St.OptionButton>
     </>
--- src/HyeEun/filter/index.tsx
+++ src/HyeEun/filter/index.tsx
@@ -13,9 +13,16 @@ interface Option {
 export default function Filter() {
   const [submitSet, setSubmitSet] = useState(new Set<string>()); // 제출할 content를 담는 Set
   // 제출하기 버튼을 눌렀을 때, alert 창 뜨게 하기
-  const handleSubmit = () => {
+  const submitFilterOption = () => {
     const submitArr = Array.from(submitSet);
     alert(submitArr);
+  };
+  // setSubmitSet handling 함수 선언
+  const handleSubmitSet = (content: string) => {
+    // submitSet에 content가 있으면 submitSet에서 삭제, 아니면 추가
+    const tempSubmitSet = new Set([...submitSet]);
+    tempSubmitSet.has(content) ? tempSubmitSet.delete(content) : tempSubmitSet.add(content);
+    setSubmitSet(tempSubmitSet);
   };
 
   return (
@@ -26,13 +33,20 @@ export default function Filter() {
             <St.OptionName>{option.name}</St.OptionName>
             <St.OptionButtonWrapper>
               {option.contents.map((content, id) => {
-                return <OptionButton key={id} content={content} submitSet={submitSet} setSubmitSet={setSubmitSet} />;
+                return (
+                  <OptionButton
+                    key={id}
+                    content={content}
+                    submitSet={submitSet}
+                    handleSubmitSet={() => handleSubmitSet(content)}
+                  />
+                );
               })}
             </St.OptionButtonWrapper>
           </St.OptionWrapper>
         );
       })}
-      <St.SubmitButton type="submit" onClick={handleSubmit}>
+      <St.SubmitButton type="submit" onClick={submitFilterOption}>
         제출하기
       </St.SubmitButton>
     </St.Root>
